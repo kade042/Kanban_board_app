7ddb38b316de45abc5eb9145be757e2c0ece36ba
kade042 <dereje88@gmail.com>
2017-06-02 22:21:44 -0600

    modified App.js and return webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { render }from 'react-dom';
 import App from './App';
-import Board from './components/Board';
-import Square from './components/Square';
-import Knight from './components/Knight';
-import { observe } from './components/Game';
+
 
 const rootEl = document.getElementById('root');
 
-observe(knightPosition =>
-  render(
-    <Board knightPosition={ knightPosition } />,
+render(
+    <App />,
     rootEl
-  )
 );

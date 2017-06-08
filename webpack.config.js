c4a80c53dfbdeb49056b6846eae22fa7d46af78c
kade042 <dereje88@gmail.com>
2017-06-08 14:47:23 -0600

    adding main parent component Kanban board

--- webpack.config.js
+++ webpack.config.js
@@ -5,30 +5,30 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './src/index'
+    './src/App',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
+      include: path.join(__dirname, 'src'),
     },
     {
       test: /\.css$/,
       loader: 'css-loader',
       options: {
         modules: true,
-        localIdentName: '[path][name]__[local]--[hash:base64:5]'
-      }
+        localIdentName: '[path][name]__[local]--[hash:base64:5]',
+      },
     },
-  ],
-  }
+   ],
+  },
 };

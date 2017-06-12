e80c11b6175da44ba160b97ec2306d990f7c2499
kade042 <dereje88@gmail.com>
2017-06-12 17:28:18 -0600

    adding and modifying tasks

--- src/components/KanbanBoardContainer.js
+++ src/components/KanbanBoardContainer.js
@@ -8,7 +8,7 @@ import 'babel-polyfill';
 const API_URL = 'http://kanbanapi.pro-react.com/';
 const API_HEADERS = {
   'Content-Type': 'application/json',
-  Authorization: 'react-pro',
+  Authorization: 'pro-react',
 };
 
 export default class KanBanboardContainer extends Component {
@@ -31,6 +31,26 @@ export default class KanBanboardContainer extends Component {
   }
 
   addTask(cardId, taskName) {
+    let cardIndex = this.state.cards.findIndex((card)=> card.id == cardId);
+    let newTask = { id: new Date(), name: taskName, done: false };
+
+    let nextState = update(this.state.cards, {
+                            [cardIndex]: {
+                              tasks: { $push: [newTask] },
+                            },
+                          });
+    this.setState({ cards: nextState });
+
+    fetch(`${API_URL}/cards/${cardId}/tasks`, {
+      method: 'post',
+      headers: API_HEADERS,
+      body: JSON.stringify(newTask),
+    })
+    .then((response)=> response.json())
+    .then((responseData)=> {
+      newTask.id = responseData.id;
+      this.setState({ cards: nextState });
+    });
 
   }
 
@@ -49,7 +69,30 @@ export default class KanBanboardContainer extends Component {
   }
 
   toggleTask(cardId, taskId, taskIndex) {
-    
+    let cardIndex = this.state.cards.findIndex((card)=>card.id == cardId);
+    let newDoneValue;
+    let nextState = update(this.state.cards, {
+                            [cardIndex]: {
+                              tasks: {
+                                [taskIndex]: {
+                                  done: {
+                                    $apply: (done) => {
+                                      newDoneValue = !done
+                                      return newDoneValue;
+                                    },
+                                  },
+                                },
+                              },
+                            },
+                          },
+                        );
+
+    this.setState({ cards: nextState });
+    fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+      method: 'put',
+      headers: API_HEADERS,
+      body: JSON.stringify({ done: newDoneValue }),
+    });
   }
 
   render() {

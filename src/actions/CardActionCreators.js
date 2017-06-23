13ebbb840909eeddfd502c995b219fdb5406e1a8
kade042 <dereje88@gmail.com>
2017-06-23 16:12:59 -0600

    adding fetchCards function in card action creators

--- src/actions/CardActionCreators.js
+++ src/actions/CardActionCreators.js
@@ -3,7 +3,13 @@ import constants from '../constants';
 import KanbanAPI from '../api/KanbanApi';
 
 let CardActionCreators = {
-
+  fetchCards() {
+    AppDispacher.dispatchAsync(KanbanAPI.fetchCards(), {
+      request: constants.FETCH_CARDS,
+      success: constants.FETCH_CARDS_SUCCESS,
+      failure: constants.FETCH_CARDS_ERROR,
+    });
+  },
 };
 
 export default CardActionCreators;

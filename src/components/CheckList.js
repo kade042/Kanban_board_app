f6495f172e94a1b5279e4ffa994f82c300601759
kade042 <dereje88@gmail.com>
2017-06-24 12:20:18 -0600

    refactor CheckList component

--- src/components/CheckList.js
+++ src/components/CheckList.js
@@ -1,9 +1,11 @@
 import React, { Component, PropTypes } from 'react';
+import TaskActionCreators from '../actions/TaskActionCreators';
 
 export default class CheckList extends Component {
   checkInputKeyPress(evt) {
     if (evt.key === 'Enter') {
-      this.props.taskCallbacks.add(this.props.cardId, evt.target.value);
+      let newTask = { id: Date.now(), name: evt.target.value, done: false };
+      TaskActionCreators.addTask(this.props.cardId, newTask);
       evt.target.value = '';
     }
   }
@@ -11,21 +13,23 @@ export default class CheckList extends Component {
   render() {
     let tasks = this.props.tasks.map((task, taskIndex) => {
       return <li key={task.id} className='checklist_task'>
-        <input type='checkbox' defaultChecked={task.done} onChange={
-            this.props.taskCallbacks.toggle.bind(null, this.props.cardId, task.id, taskIndex)
-          }/>
-        {task.name}
-        <a href='#' className='checklist_task--remove' onClick={
-            this.props.taskCallbacks.delete.bind(null, this.props.cardId, task.id, taskIndex)
-          }/>
+        <input type='checkbox'
+               Checked={task.done}
+               onChange={
+                TaskActionCreators.toggleTask.bind(null, this.props.cardId, task.id, taskIndex)
+          } />
+        {task.name}{' '}
+        <a href='#'
+           className='checklist_task--remove'
+           onClick={
+            TaskActionCreators.deleteTask.bind(null, this.props.cardId, task.id, taskIndex)
+          } />
       </li>;
     });
 
     return (
       <div className='checklist'>
-        <ul>
-          {tasks}
-        </ul>
+        <ul>{tasks}</ul>
         <input  type='text'
                 className='checklist--add-task'
                 placeholder='Type then hit Enter to add a task'
@@ -38,5 +42,4 @@ export default class CheckList extends Component {
 CheckList.propTypes = {
   cardId: PropTypes.number,
   tasks: PropTypes.arrayOf(PropTypes.object),
-  taskCallbacks: PropTypes.object,
 };
